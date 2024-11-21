b81ba8dbd47692d5e8da2b673f74fd37dd6cbee4
Your Name <you@example.com>
2024-11-21 14:22:12 +0100

    search page

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
-
+import Content from '../components/Content'
+import { useState } from 'react'
+import { useEffect } from 'react'
 export const SearchPage = () => {
+  const [search, setSearch] = useState("Arcane")
+  const [searchUrl, setSearchUrl] = useState(`https://api.themoviedb.org/3/search/multi?api_key=${import.meta.env.VITE_API_KEY}&query=Arcane`)
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+    setSearchUrl(`https://api.themoviedb.org/3/search/multi?api_key=${import.meta.env.VITE_API_KEY}&query=${search}`)
+  }
+  
   return (
-    <div>SearchPage</div>
+    <div className='m-5'>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" value={search} onChange={handleSearch}/>
+      <Content url={searchUrl} queryKey="search"/>
+    </div>
   )
 }
